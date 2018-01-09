e0d5e863786626f208c8667362ff7b2312f91d71
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2018-01-09 17:37:20 -0500

    progress....

---
+++ project1/saas-tutorial/config/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret': 'zWnZHaE5bej5%S2Rxdwb@Pw@SD',  
+    'database': 'mongodb://127.0.0.1:27017/saas-tutorial'
+}
--- project1/saas-tutorial/server.js
+++ project1/saas-tutorial/server.js
@@ -2,7 +2,32 @@
 const express = require('express');
 app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const passport = require('passport');
+const config = require('./config/main');
+const jwt = require('jsonwebtoken');
+
 const port = 3000;
+
+// Use body-parser to get POST requests for API use
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Log requests to the console
+app.use(morgan('dev'));
+
+mongoose.connect(config.database);
+
+
+// We will add a quick home page route so I can give a quick demonstration of what morgan does. 
+// Add this next.
+
+
+// Home route. We'll end up changing this to our main front end index later.
+app.get('/', function(req, res) {  
+    res.send('Relax. We will put the home page here later.');
+  });
 
 app.listen(port);
 console.log('Your server is running on port '+ port +'.');
